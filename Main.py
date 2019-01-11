75d6d37c4493d4c64fbedb909f25e2f25d36897b
noobpersons <46582865+noobpersons@users.noreply.github.com>
2019-01-10 20:57:59 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,40 +1,90 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+import time
+import random
+from discord import Game
+from itertools import cycle
+import json
 import os
+import bs4, requests
+from time import gmtime, strftime
+from discord import opus
 import youtube_dl
+client = commands.Bot(command_prefix=("s."))
+status = ["testing the bot", "s.help"]
 
-client = commands.Bot(command_prefix='!')
-songs = asyncio.Queue()
-play_next_song = asyncio.Event()
-
+async def change_status():
+  await client.wait_until_ready()
+  msgs = cycle(status)
+  
+  while not client.is_closed:
+    current_status = next(msgs)
+    await client.change_presence(game=discord.Game(name=current_status))
+    await asyncio.sleep(5)
+    
+players = {}	
 
 @client.event
 async def on_ready():
-    print('client ready')
+    print('Logged in as')
+    print("User name:", client.user.name)
+    print("User id:", client.user.id)
+    print('---------------')
 
-
-async def audio_player_task():
-    while True:
-        play_next_song.clear()
-        current = await songs.get()
-        current.start()
-        await play_next_song.wait()
-
-
-def toggle_next():
-    client.loop.call_soon_threadsafe(play_next_song.set)
-
+@client.event
+async def on_message(message):
+  if message.content == 'm.skip':
+      serverid = message.server.id
+      players[serverid].stop()
+  if message.content == 'm.pause':
+      serverid = message.server.id
+      players[serverid].pause()
+      await client.send_message(message.channel, "Player paused")
+  if message.content == 'm.resume':
+      serverid = message.server.id
+      players[serverid].resume()
+      await client.send_message(message.channel, "Player resumed")
+  if message.content.startswith('m.play '):
+      author = message.author
+      name = message.content.replace("m.play ", '')                 
+      fullcontent = ('http://www.youtube.com/results?search_query=' + name)
+      text = requests.get(fullcontent).text
+      soup = bs4.BeautifulSoup(text, 'html.parser')
+      img = soup.find_all('img')
+      div = [ d for d in soup.find_all('div') if d.has_attr('class') and 'yt-lockup-dismissable' in d['class']]
+      a = [ x for x in div[0].find_all('a') if x.has_attr('title') ]
+      title = (a[0]['title'])
+      a0 = [ x for x in div[0].find_all('a') if x.has_attr('title') ][0]
+      url = ('http://www.youtube.com'+a0['href'])
+      delmsg = await client.send_message(message.channel, 'Now Playing ** >> ' + title + '**')
+      server = message.server
+      voice_client = client.voice_client_in(server)
+      player = await voice_client.create_ytdl_player(url)
+      players[server.id] = player
+      print("User: {} From Server: {} is playing {}".format(author, server, title))
+      player.start()
+  await client.process_commands(message)
 
 @client.command(pass_context=True)
-async def play(ctx, url):
-    if not client.is_voice_connected(ctx.message.server):
-        voice = await client.join_voice_channel(ctx.message.author.voice_channel)
-    else:
-        voice = client.voice_client_in(ctx.message.server)
+async def ping(ctx):
+    pingtime = time.time()
+    pingms = await client.say("Pinging...")
+    ping = (time.time() - pingtime) * 1000
+    await client.edit_message(pingms, "Pong! :ping_pong: ping time is `%dms`" % ping)
+    
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
+    await client.say('Connected to voice channel: **[' + str(channel) + ']**')
+	
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
+    await client.say('Left voice channel')
 
-    player = await voice.create_ytdl_player(url, after=toggle_next)
-    await songs.put(player)
-
-client.loop.create_task(audio_player_task())
 client.run(os.environ['BOT_TOKEN'])
